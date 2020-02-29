8f758258fa3e2caf99a21c872207b56bf49f48ff
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-29 20:12:00 +0100

    Move files to a new structure (#241)
    * Moved around all the code to a new folders
    
    * Some bugfixes
    
    * Fixed meta files
    
    * ci fix
    
    * cu
    
    * cs
    
    *         "league/mime-type-detection": "dev-master",
    
    * flysystem fix
    
    * pslam fix
    
    * cs
    
    * cs

---
+++ tests/Unit/Input/DescribeStackEventsInputTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AsyncAws\CloudFormation\Tests\Unit\Input;
+
+use AsyncAws\CloudFormation\Input\DescribeStackEventsInput;
+use AsyncAws\Core\Test\TestCase;
+
+class DescribeStackEventsInputTest extends TestCase
+{
+    /**
+     * @see https://docs.aws.amazon.com/AWSCloudFormation/latest/APIReference/API_DescribeStackEvents.html#API_DescribeStackEvents_Errors
+     */
+    public function testRequestBody(): void
+    {
+        $input = new DescribeStackEventsInput([
+            'StackName' => 'MyStack',
+        ]);
+
+        $expected = '
+            Action=DescribeStackEvents
+            &Version=2010-05-15
+            &StackName=MyStack
+        ';
+
+        self::assertHttpFormEqualsHttpForm($expected, $input->requestBody());
+    }
+}
---
+++ tests/Unit/Input/DescribeStacksInputTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AsyncAws\CloudFormation\Tests\Unit\Input;
+
+use AsyncAws\CloudFormation\Input\DescribeStacksInput;
+use AsyncAws\Core\Test\TestCase;
+
+class DescribeStacksInputTest extends TestCase
+{
+    public function testRequestBody(): void
+    {
+        $input = new DescribeStacksInput([
+            'StackName' => 'foo',
+            'NextToken' => 'bar',
+        ]);
+
+        // see https://docs.aws.amazon.com/CloudFormation/latest/APIReference/API_DescribeStacks.html
+        $expected = '
+            Action=DescribeStacks
+            &Version=2010-05-15
+            &StackName=foo
+            &NextToken=bar
+        ';
+
+        self::assertHttpFormEqualsHttpForm($expected, $input->requestBody());
+    }
+}
